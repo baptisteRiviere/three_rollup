f1a631dede3896df7aede6334a338d45efccbaa2
baptisteRiviere <riviereb85@gmail.com>
2023-02-17 16:16:03 +0100

    ajout du sol

--- src/main.js
+++ src/main.js
@@ -11,12 +11,14 @@ let playerDirection = new THREE.Vector3();
 let targetCollider;
 const worldOctree = new Octree();
 
-const STEPS_PER_FRAME = 4;
+const STEPS_PER_FRAME = 2;
 const SPHERE_RADIUS = 0.2;
 const GRAVITY = 60;
-const BUCKET_POSITION = new THREE.Vector3(0, -2, -6);
+const FLOOR_HEIGHT = -3;
+const BUCKET_POSITION = new THREE.Vector3(0, FLOOR_HEIGHT, -8);
 const TARGET_SPHERE_RADIUS = 1.2;
 let IMPULSE = 30;
+let NB_TRIES = 0;
 
 // CONTAINER - SCENE - CAMERA - OCTREE
 
@@ -80,8 +82,7 @@ scene.add(sphereMesh);
 let sphere = {
   mesh: sphereMesh,
   collider: new THREE.Sphere(new THREE.Vector3(3, 5, 2), SPHERE_RADIUS),
-  velocity: new THREE.Vector3(),
-  hold: true
+  velocity: new THREE.Vector3()
 };
 
 // CONTROLLER 
@@ -93,17 +94,20 @@ scene.add(controller);
 // EVENT LISTENER
 window.addEventListener('resize', onWindowResize);
 
-// SPECIFICS FUNCTION TO THREE EXAMPLE
 
-const geometry = new THREE.CylinderGeometry(0, 0.05, 0.2, 32).rotateX(Math.PI / 2);
+// FLOOR
 
-function onSelect() {
-  const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
-  const mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(0, 0, - 0.3).applyMatrix4(controller.matrixWorld);
-  mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
-  scene.add(mesh);
-}
+var geometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var floor = new THREE.Mesh(geometry, material);
+floor.material.side = THREE.DoubleSide;
+floor.rotation.x = - Math.PI / 2;
+floor.position.y = FLOOR_HEIGHT;
+floor.visible = false;
+scene.add(floor);
+worldOctree.fromGraphNode(floor);
+
+// WINDOW RESIZE
 
 function onWindowResize() {
 
@@ -117,9 +121,11 @@ function onWindowResize() {
 // PHYSICS
 
 function throwBall() {
-  sphere.hold = false;
+  NB_TRIES += 1;
+  sphere.visible = true;
   let elements = controller.matrixWorld.elements;
   playerDirection = new THREE.Vector3(-elements[8], -elements[9], -elements[10]).normalize()
+  playerDirection.y += 0.5; // permit to give a smooth curve to the ball
   sphere.collider.center.copy(camera.position)
   sphere.velocity.copy(playerDirection).multiplyScalar(IMPULSE);
   sphere.collider.center.set(0, 0, - 0.3).applyMatrix4(controller.matrixWorld);
@@ -149,15 +155,10 @@ function targetReached(target) {
   return (d2 < r2);
 }
 
-
-function groundIntersected() {
-  return (sphere.collider.center.y - sphere.collider.radius < -3);
-}
-
 function returnSphereToPlayer() {
   sphere.collider.center.copy(camera.position);
   sphere.mesh.position.copy(sphere.collider.center);
-  sphere.hold = true;
+  sphere.visible = false;
 }
 
 // ACTION
@@ -180,18 +181,14 @@ function render() {
 
   const deltaTime = Math.min(0.05, clock.getDelta()) / STEPS_PER_FRAME;
 
-  if (!sphere.hold) {
+  if (sphere.visible) {
     updateSphere(deltaTime);
-    if (groundIntersected(sphere)) {
-      returnSphereToPlayer();
-    };
   }
 
   if (targetReached(targetCollider)) {
-    console.log("oui")
-    //spawn(2, 5, 5);
-    //message.innerText = "Congratulations ! As I see you would have been the best bucketball player in the middle age. Play as you want !";
-    //menuPanel.style.display = 'block';
+    returnSphereToPlayer();
+    console.log("You won by trying " + NB_TRIES + " times, continue training to become the new bucketball great master");
+    NB_TRIES = 0;
   };
 
   renderer.render(scene, camera);
